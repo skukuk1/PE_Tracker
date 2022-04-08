a8a68b4fc42c01d0c3ff05891c182a770dfd0fd0
Samantha Kukuk <skukuk1@student.cvtc.edu>
2022-04-08 16:44:18 -0500

    Removed Temporary support for creating tables.

--- index.js
+++ index.js
@@ -33,38 +33,6 @@ express()
       const client = await pool.connect();
       const tables = await client.query(
       `
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        email text NOT NULL,
-        password text NOT NULL
-      );
-    
-      CREATE TABLE students (
-          id SERIAL PRIMARY KEY,
-          name TEXT NOT NULL,
-          school INT NOT NULL,
-          expires DATE NOT NULL
-      );
-    
-      CREATE TABLE schools (
-          id SERIAL PRIMARY KEY,
-          name TEXT NOT NULL,
-          address TEXT NOT NULL
-      );
-      
-      CREATE TABLE observations (
-          id SERIAL PRIMARY KEY,
-          users_id INT NOT NULL,
-          students_id INT NOT NULL,
-          tasks_id INT NOT NULL,
-          duration INTERVAL NOT NULL
-      );
-      
-      CREATE TABLE tasks (
-          id SERIAL PRIMARY KEY,
-          name TEXT NOT NULL
-      );
-
       SELECT c.relname AS table, a.attname AS column, t.typname AS type
       FROM pg_catalog.pg_class AS c
       LEFT JOIN pg_catalog.pg_attribute AS a
