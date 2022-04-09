722f2182f6a3b0b0f97e8cd86ad69b31914f475c
Samantha Kukuk <skukuk1@student.cvtc.edu>
2022-04-08 22:21:18 -0500

    [Finishes#181783436] Validate the technology stack.

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ express()
     try {
       const client =  await pool.connect();
 
+      const tasks = await client.query(
+        'SELECT * FROM tasks ORDER BY id ASC');
+      
+      const locals = {
+        'tasks': (tasks) ? tasks.rows : null
+      };
+      res.render('pages/index', locals);
       client.release();
-      res.send("Works");
     }
     catch (err) {
       console.error(err);
@@ -43,11 +49,42 @@ express()
       ORDER BY c.relname, a.attnum;
       `);
 
+      const obs = await client.query(
+        `SELECT * FROM observations`);
+
       const locals = {
-        'tables': (tables) ? tables.rows: null
+        'tables': (tables) ? tables.rows: null,
+        'obs': (obs) ? obs.rows: null
       };
 
       res.render('pages/db-info', locals);
+      client.release();
+    }
+    catch (err) {
+      console.error(err);
+      res.send("Error: " + err);
+    }
+  })
+  .post('/log', async(req, res) => {
+    try {
+      const client = await pool.connect();
+      const usersId = req.body.users_id;
+      const studentsId = req.body.students_id;
+      const tasksId = req.body.tasks_id;
+      const duration = req.body.duration;
+      const sqlInsert = await client.query(
+        `INSERT INTO observations (users_id, students_id, tasks_id, duration)
+        VALUES (${usersId}, ${studentsId}, ${tasksId}, ${duration})
+        RETURNING id as new_id;`);
+      console.log(`Tracking task ${tasksId}`);
+
+      const result = {
+        'response': (sqlInsert) ? (sqlInsert.rows[0]) : null
+      };
+      res.set({
+        'Content-Type': 'application/json'
+      });
+      res.json({ requestBody: result});
       client.release();
     }
     catch (err) {
